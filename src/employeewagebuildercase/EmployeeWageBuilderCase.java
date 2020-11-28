71f3e4e5e62934e6f01001d3bb2e753933acfa86
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-29 02:19:17 +0530

    Refactore: Wage computation using method

--- src/employeewagebuildercase/EmployeeWageBuilderCase.java
+++ src/employeewagebuildercase/EmployeeWageBuilderCase.java
@@ -14,6 +14,45 @@ public class EmployeeWageBuilderCase {
     /**
      * @param args the command line arguments
      */
+    
+    //uc7 method code here
+    public void ComputeWage()
+    {
+	int ratePerHour = 20;
+	int empHour = 0;
+	int NUM_WORKING_DAYS = 20;
+	int MAX_HRS_IN_MONTH = 100;
+	int totalEmpHrs = 0;
+	int totalWorkingDays = 0;
+	
+        while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) 
+        {
+            totalWorkingDays++;
+            totalEmpHrs++;	
+            int randomNumber = (int) Math.floor(Math.random() * 10) % 3;
+            
+            switch (randomNumber) 
+            {
+		case 0:
+                    System.out.println("employee is full-time");
+                    empHour = 8;
+                    int salary = (empHour * ratePerHour);
+                    System.out.println("salary is :" + salary);
+                    break;
+		case 1:
+                    System.out.println("employee is part-time");
+                    empHour = 4;
+                    int salary1 = (empHour * ratePerHour);
+                    System.out.println("salary is :" + salary1);
+                    break;
+		default:
+                    System.out.println("employee is absent");
+            }
+	}
+    }//uc7 end
+    
+    
+    
     public static void main(String[] args) {
         
         System.out.println("***** Welcome to Employee Wage Computation Program *****");
@@ -132,6 +171,11 @@ public class EmployeeWageBuilderCase {
         }
         totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("TOTLA EMP WAGE: "+totalEmpWage);
+        
+        
+        //uc7. Refactor the Code to write a Class Method to Compute Employee Wage
+        EmployeeWageBuilderCase obj1 = new EmployeeWageBuilderCase();
+        obj1.ComputeWage();
         
     }
     
