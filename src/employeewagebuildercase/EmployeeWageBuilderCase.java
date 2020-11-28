456238dae6b80aa0e5c6a2abe6df99c8d79b376f
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-29 02:07:02 +0530

    Added: Calculating employee wage for month

--- src/employeewagebuildercase/EmployeeWageBuilderCase.java
+++ src/employeewagebuildercase/EmployeeWageBuilderCase.java
@@ -21,6 +21,9 @@ public class EmployeeWageBuilderCase {
         int IS_FULL_TIME = 1;
         int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
+        int totalWorkingDays = 0;
+        int NUM_WORKING_DAYS = 2;
+        int totalEmpWage = 0;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         
         int empHrs = 0;
@@ -79,6 +82,30 @@ public class EmployeeWageBuilderCase {
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("EMP WAGE: "+empWage);
+        
+        
+        //uc5. calculating wage for month
+        for(int day=0; day<NUM_WORKING_DAYS; day++)
+        {
+            int empCheck1 = (int) Math.floor(Math.random() * 10) % 3;
+            switch(empCheck1)
+            {
+                case 1:
+                    //1 = FULL TIME
+                    empHrs = 8;
+                    break;
+                case 2: 
+                    //2 = PART TIME
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }   
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            System.out.println("EMP WAGE: "+empWage);
+        }
+        System.out.println("TOTAL EMP WAGE: "+totalEmpWage);
         
     }
     
