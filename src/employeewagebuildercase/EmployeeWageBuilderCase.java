46e5295d78b8f5ca1a52ff2fc20d8b14ac8f88da
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-29 01:54:46 +0530

    Added: Emp wage calculation

--- src/employeewagebuildercase/EmployeeWageBuilderCase.java
+++ src/employeewagebuildercase/EmployeeWageBuilderCase.java
@@ -19,7 +19,11 @@ public class EmployeeWageBuilderCase {
         System.out.println("***** Welcome to Employee Wage Computation Program *****");
         
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        
+        int empHrs = 0;
+        int empWage = 0;
         
         //uc1. employee present or absent
 	if(empCheck == IS_FULL_TIME) 
@@ -30,6 +34,19 @@ public class EmployeeWageBuilderCase {
         {
             System.out.println("Employee is absent");
 	}
+        
+        
+        //uc2. employee wage calculation
+        if(empCheck == IS_FULL_TIME) 
+        {
+            empHrs = 8;
+	}
+        else 
+        {
+            empHrs = 0;
+	}
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("EMP WAGE: "+empWage);
         
     }
     
